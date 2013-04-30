81b1fd53b67d491d77842a628590f4183729057a
Shawn Pearce <sop@google.com>
2013-04-29 17:16:42 -0700

    Reset compressed size when combining JAR/ZIP archives
    zlib does not always deflate a resource to the same compressed size as
    the original input.  The input resource may have been compressed using
    a different version of libz, or different compression settings.
    
    Before adding each entry to the ZIP, clone the entry and reinitialize
    the compressed field to -1 as is done by the more commonly used
    ZipEntry(String) constructor.
    
    This fixes a rare case where including a JAR in a java_binary() may
    throw an error such as:
    
      invalid entry compressed size (expected 4271 but got 4275 bytes)

--- src/com/facebook/buck/java/JarDirectoryStep.java
+++ src/com/facebook/buck/java/JarDirectoryStep.java
@@ -203,7 +203,9 @@ public class JarDirectoryStep implements Step {
         continue;
       }
 
-      jar.putNextEntry(entry);
+      ZipEntry newEntry = new ZipEntry(entry);
+      newEntry.setCompressedSize(-1);
+      jar.putNextEntry(newEntry);
       InputStream inputStream = zip.getInputStream(entry);
       ByteStreams.copy(inputStream, jar);
       jar.closeEntry();
